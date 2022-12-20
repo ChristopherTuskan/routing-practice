14083398efa433e7005a1e7e519ca501de696ea8
Chris Tuskan <ctuskan@gmail.com>
2022-12-20 18:13:21 -0500

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import { BrowserRouter, Link, Routes, Route, useParams, useNavigate } from 'react-router-dom';
 import './App.css';
+
+const Home = (props) => {
+  return (
+    <h1>Welcome!</h1>
+  )
+}
+
+const NumOrWord = (props) => {
+  const {numOrWord} = useParams();
+  console.log(numOrWord);
+  if (isNaN(+numOrWord) === false){
+    return (<h1 style={{textAlign: "center"}}>The number is: {numOrWord}</h1>);
+  }else{
+    return (<h1 style={{textAlign: "center"}}>The word is: {numOrWord}</h1>);
+  }
+}
+
+const WordBG = (props) => {
+  const {word, textColor, bgColor} = useParams();
+    return (
+      <h1 style={{color: textColor, backgroundColor: bgColor, textAlign: "center"}}>This word is: {word}</h1>
+    )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/:numOrWord" element={<NumOrWord />} />
+        <Route path="/:word/:textColor/:bgColor" element={<WordBG />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
